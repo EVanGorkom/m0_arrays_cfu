b3e186a658e9f78e0ff8a1db6f9c07eef3ae680e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-06-21 03:07:29 -0500

    Define four different arrays

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+#Check for understanding: Arrays
+
+names = ["Ethan", "Sarah", "Kaitlyn", "Brendan"]
+ages = [13, 34, 28, 64]
+bank_statements = [1.00, 12.05, 10000.78, 234.92]
+booleans = [true, false, false, true]
+
